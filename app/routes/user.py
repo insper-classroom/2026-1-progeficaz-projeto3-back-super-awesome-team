60fad6e7845dc6b74366514f254c9f0d0d8b34c7
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-24 19:07:40 -0300

    feat: user schema validation and user model structure

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, request, jsonify
-from app.extensions import mongo
+from ..extensions import mongo
+from ..models.user import User
+from ..schemas.user_schema import UserSchema
 
+schema = UserSchema()
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/user', methods=['POST'])
 def create_user():
+    erros = schema.validate(request.get_json())
+    if erros:
+        return jsonify(erros), 400
+
     data = request.get_json()
-    result = mongo['users'].insert_one(data)
+    user = User(data["name"], data["email"], data["password"])
+    mongo['users'].insert_one(user.to_dictionary())
     return jsonify({'message': 'OK ✅'}), 201
 
 @user_bp.route('/user', methods=['GET'])
