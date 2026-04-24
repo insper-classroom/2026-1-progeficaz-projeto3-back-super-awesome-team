e71b43a194162763ff71a94634d5676f625f7cbe
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-24 19:43:56 -0300

    feat: migrating business rules (route logic) to services

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,25 +1,21 @@
 from flask import Blueprint, request, jsonify
 from ..extensions import mongo
 from ..models.user import User
-from ..schemas.user_schema import UserSchema
+from ..services.user_services import create_user_service
 
-schema = UserSchema()
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/user', methods=['POST'])
 def create_user():
-    erros = schema.validate(request.get_json())
-    if erros:
-        return jsonify(erros), 400
-
     data = request.get_json()
-    user = User(data["name"], data["email"], data["password"])
-    mongo['users'].insert_one(user.to_dictionary())
-    return jsonify({'message': 'OK ✅'}), 201
+    result, error = create_user_service(data)
+    if error:
+        return jsonify(error), 400
+    return jsonify(result), 201
 
 @user_bp.route('/user', methods=['GET'])
 def get_users():
     users = list(mongo['users'].find())
     for user in users:
-        user['_id'] = str(user['_id'])  # converte ObjectId para string
+        user['_id'] = str(user['_id'])
     return jsonify(users), 200
