efe3a75b4d8d46497966fb438dc5d46edf71427d
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-27 17:36:02 -0300

    fix: reading environment variables outside of the email sending function

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -7,10 +7,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+CLIENT_ID = str(os.getenv("CLIENT_ID"))
+CLIENT_SECRET = str(os.getenv("CLIENT_SECRET"))
+REFRESH_TOKEN = str(os.getenv("REFRESH_TOKEN"))
+
 def enviar_email(subject, body, to_email):
-    CLIENT_ID = str(os.getenv("CLIENT_ID"))
-    CLIENT_SECRET = str(os.getenv("CLIENT_SECRET"))
-    REFRESH_TOKEN = str(os.getenv("REFRESH_TOKEN"))
     creds = Credentials(
         token=None,
         refresh_token=REFRESH_TOKEN,
