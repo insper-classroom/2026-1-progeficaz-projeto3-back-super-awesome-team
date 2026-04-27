b2625f9dc0f6b9d9e9d9274c5207285cc4e3c6e3
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-27 16:22:54 -0300

    feat: protect GET /user with JWT middleware and hide password field

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from ..extensions import mongo
 from ..models.user import User
 from ..services.user_services import create_user_service
+from ..utils.jwt_utils import jwt_required
 
 user_bp = Blueprint('user', __name__)
 
@@ -14,8 +15,11 @@ def create_user():
     return jsonify(result), 201
 
 @user_bp.route('/user', methods=['GET'])
+@jwt_required
 def get_users():
     users = list(mongo['users'].find())
     for user in users:
         user['_id'] = str(user['_id'])
+        user.pop('password', None)
+        user.pop('senha', None)
     return jsonify(users), 200
