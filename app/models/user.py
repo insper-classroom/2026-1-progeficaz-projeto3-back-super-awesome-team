c8b2786141d5f52c5c681b9d02b0ea18f671ba2c
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-29 21:46:42 -0300

    feat: add is_verified and verification_token to User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,19 @@
+import uuid
+
+
 class User:
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+        self.is_verified = False
+        self.verification_token = str(uuid.uuid4())
 
     def to_dictionary(self):
-        return {"name": self.name, "email": self.email, "password": self.password}
+        return {
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "is_verified": self.is_verified,
+            "verification_token": self.verification_token,
+        }
