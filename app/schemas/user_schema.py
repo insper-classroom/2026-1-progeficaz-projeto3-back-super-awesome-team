6221a43d065605e78c3d346d30b4f6cd66004e91
miqueias-ayron <miqueiasamf@al.insper.edu.br>
2026-04-29 21:31:31 -0300

    feat: add confirm_password field to UserSchema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,10 +1,18 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields, validate, validates_schema, ValidationError
 
 
 class UserSchema(Schema):
     name = fields.Str(required=True)
     email = fields.Email(required=True)
     password = fields.Str(required=True, validate=validate.Length(min=6))
+    confirm_password = fields.Str(required=True)
+
+    @validates_schema
+    def validate_passwords_match(self, data, **kwargs):
+        if data.get("password") != data.get("confirm_password"):
+            raise ValidationError(
+                "As senhas não coincidem.", field_name="confirm_password"
+            )
 
 
 class UpdateUserSchema(Schema):
